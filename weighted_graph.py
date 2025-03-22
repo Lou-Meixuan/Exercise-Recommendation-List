d16e745354c126bad4607eba65f53b9087f4990b
Your Namegit <zimohuang0@gmail.com>
2025-03-22 15:38:31 -0400

    added base Exercise, Vertex, Graph classes

--- weighted_graph.py
+++ weighted_graph.py
@@ -1,4 +1,4 @@
-from typing import annotations
+from typing import List, Optional
 class Exercise:
     """An exercise and its categories
 
@@ -25,15 +25,15 @@ class Exercise:
     name: str
     force: str
     level: str
-    mechanic: str
-    equipment: str
-    primaryMuscles: str
-    secondaryMuscles: str
+    mechanic: Optional[str]
+    equipment: Optional[str]
+    primaryMuscles: List[str]
+    secondaryMuscles: List[str]
     instructions: str
     category: str
     images: List[str]
 
-    def __init__(self, id: int, name: str, force: str, level: str, mechanic: str, equipment: str, primaryMuscles: str, secondaryMuscles: str, instructions: str, category: str, images: List[str]) -> None:
+    def __init__(self, id, name, force, level, mechanic, equipment, primaryMuscles, secondaryMuscles, instructions, category, images) -> None:
         self.id = id
         self.name = name
         self.force = force
@@ -45,3 +45,42 @@ class Exercise:
         self.instructions = instructions
         self.category = category
         self.images = images
+
+class Vertex:
+    """A vertex or a node in the graph holding an item of the exercise class"
+    
+    instance Attributes:
+    - item: The item of the exercise class
+    - neighbours: The neighbours of the node"
+    """
+    item: Exercise
+    neighbours: List["Vertex"] # Fwd reference string to avoid circular import
+
+    def __init__(self, item: Exercise) -> None:
+        self.item = item
+        self.neighbours = []
+    
+    def add_neighbour(self, neighbour: Vertex) -> None:
+        self.neighbours.append(neighbour)
+        neighbour.neighbours.append(self)
+    
+    def remove_neighbour(self, neighbour: Vertex) -> None:
+        self.neighbours.remove(neighbour)
+        neighbour.neighbours.remove(self)
+
+
+class Graph:
+    """A weighted graph holding the vertices of the exercise class"
+    
+    Instance Attributes:
+    - _vertices: The vertices of the graph
+    - edges: The edges of the graph
+    """
+
+    _vertices: List[Vertex]
+    
+    def __init__(self) -> None:
+        self._vertices = []
+    
+    def add_vertex(self, exercise: Exercise) -> None: # not sure about here
+        self._vertices.append(Vertex(exercise))
