277f003f7edc159df9ecf9d1750e76c30815db4c
Lou-Meixuan <meixuan.lou@mail.utoronto.ca>
2025-03-25 21:41:21 -0400

    Fix the Exercise class

--- weighted_graph.py
+++ weighted_graph.py
@@ -1,50 +1,56 @@
 from typing import List, Optional
+
+
 class Exercise:
     """An exercise and its categories
 
     Instance Attributes:
-    - id: A unique identifier for the exercise
+    - user_id: A unique identifier for the exercise
     - name: The name of the exercise
     - force: The force the exercise targets
     - level: The level of the exercise
     - mechanic: The mechanic of the exercise
     - equipment: The equipment needed for the exercise
-    - primaryMuscles: The primary muscles targeted by the exercise
-    - secondaryMuscles: The secondary muscle(s) targeted by the exercise
+    - primary_muscles: The primary muscles targeted by the exercise
+    - secondary_muscles: The secondary muscle(s) targeted by the exercise
     - instructions: The instructions for the exercise
     - category: The category of the exercise
-    - images: The images of the exercise    
-    
+    - images: The images of the exercise
+
 
     Representation Invariants:
     - self.id > 0
     - self.name != ""
     """
 
-    id: int
+    user_id: int
     name: str
     force: str
     level: str
     mechanic: Optional[str]
     equipment: Optional[str]
-    primaryMuscles: List[str]
-    secondaryMuscles: List[str]
+    primary_muscles: list[str]
+    secondary_muscles: list[str]
     instructions: str
     category: str
-    images: List[str]
+    images: list[str]
 
-    def __init__(self, id, name, force, level, mechanic, equipment, primaryMuscles, secondaryMuscles, instructions, category, images) -> None:
-        self.id = id
+    def __init__(self, user_id, name, force, level, mechanic, equipment, primary_muscles, secondary_muscles,
+                 instructions,
+                 category, images) -> None:
+        """Initialize an exercise with the given properties"""
+        self.user_id = user_id
         self.name = name
         self.force = force
         self.level = level
         self.mechanic = mechanic
         self.equipment = equipment
-        self.primaryMuscles = primaryMuscles
-        self.secondaryMuscles = secondaryMuscles
+        self.primary_muscles = primary_muscles
+        self.secondary_muscles = secondary_muscles
         self.instructions = instructions
         self.category = category
         self.images = images
+
 
 class Vertex:
     """A vertex or a node in the graph holding an item of the exercise class"
