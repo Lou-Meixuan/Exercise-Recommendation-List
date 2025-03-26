01b08c69a8a574cef53309531cd47189db5ec88f
Lou-Meixuan <meixuan.lou@mail.utoronto.ca>
2025-03-26 01:05:47 -0400

    add the function that load the graph

--- similarity_score_calculation.py
+++ similarity_score_calculation.py
@@ -61,7 +61,7 @@ class _Vertex:
     - item: The item of the exercise class
     - neighbours: All the vertices that adjacent to this vertex
     """
-    item: Exercise
+    item: Any
     neighbours: set[_Vertex]
     kind:str
 
@@ -225,6 +225,25 @@ class Graph:
         else:
             return False
 
+def load_whole_graph(filename:str) -> Graph:
+    """Return an exercise graph corresponding to the given datasets.
+
+    Preconditions:
+        - filename is the path to a html file corresponding to the exercise data
+        - each book ID in reviews_file exists as a book ID in book_names_file
+    """
+    graph = Graph()
+    name_list = ["force", "level", "mechanic", "equipment", "primary_muscles", "secondary_muscles"]
+    with open(filename, 'r') as f:
+        html_string = f.read()
+        for row in html_string:
+            exercise_name = row[1]
+            graph.add_vertex(exercise_name, "name")
+            for index in range(6):
+                prop = row[index + 2]
+                graph.add_vertex(prop, name_list[index])
+                graph.add_edge(exercise_name, prop)
+    return graph
 
 if __name__ == '__main__':
     import doctest
