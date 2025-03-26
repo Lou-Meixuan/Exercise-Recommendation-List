78692e7efc4e902ca7a16bf60c21d6b26942b7f0
Lou-Meixuan <meixuan.lou@mail.utoronto.ca>
2025-03-26 14:41:36 -0400

    ...

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from similarity_score_calculation import Graph
+import os
+import json
 
 def load_whole_graph(filename:str) -> Graph:
     """Return an exercise graph corresponding to the given datasets.
@@ -16,6 +18,19 @@ def load_whole_graph(filename:str) -> Graph:
             graph.add_vertex(exercise_name, "name")
             for index in range(6):
                 prop = row[index + 2]
-                graph.add_vertex(prop, name_list[index])
-                graph.add_edge(exercise_name, prop)
+                if type(prop) is list:
+                    load_lists(prop, exercise_name, graph, index)
+                elif prop is not None:
+                    graph.add_vertex(prop, name_list[index])
+                    graph.add_edge(exercise_name, prop)
     return graph
+
+def load_lists(prop:list[str], exercise_name:str, graph:Graph, index:int)-> None:
+    if index == 4:
+        for each in prop:
+            graph.add_vertex(prop, "primary_muscles")
+            graph.add_edge(exercise_name, prop)
+    if index == 5:
+        for each in prop:
+            graph.add_vertex(prop, "secondary_muscles")
+            graph.add_edge(exercise_name, prop)
