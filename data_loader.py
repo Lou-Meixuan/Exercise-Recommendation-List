9d586e79e6f8a26104876b8564427cacbbbf5e4c
Lou-Meixuan <meixuan.lou@mail.utoronto.ca>
2025-03-28 15:34:08 -0400

    finished data_loader

--- data_loader.py
+++ data_loader.py
@@ -1,6 +1,18 @@
-import csv
-from typing import Optional
+"""CSC111 Winter 2025 Project2: Recommendation System For Gym Exercises
 
+Module Description
+==================
+This module contains the Exercise classes, and the functions for our project
+
+Copyright and Usage Information
+===============================
+This file is created solely by students (Meixuan Lou, Zimo Huang, Hongyi Mei, Yunji Hwang) taking CSC111 at the
+University of Toronto St. George campus. All forms of distribution of this code, whether as given or with any changes,
+are expressly prohibited.
+
+This file is Copyright (c) 2025 CSC111 Student Team: Meixuan Lou, Zimo Huang, Hongyi Mei, Yunji Hwang
+"""
+import csv
 from similarity_score_calculation import Graph
 
 
@@ -9,12 +21,10 @@ class Exercise:
     An exercise and its categories
 
     Instance Attributes:
-    - name: The name of the exercise
-    - force: The force the exercise targets
-    - level: The level of the exercise
-    - mechanic: The mechanic of the exercise
-    - equipment: The equipment needed for the exercise
-    - muscles: The primary and secondary muscle(s) targeted by the exercise
+    - properties: A list contains: name (The name of the exercise), force (The force the exercise targets), 
+                level (The level of the exercise), mechanic (The mechanic of the exercise), 
+                equipment (The equipment needed for the exercise), 
+                muscles (The primary and secondary muscle(s) targeted by the exercise)
     - instructions: The instructions for the exercise
     - category: The category of the exercise
     - images: The images of the exercise
@@ -23,33 +33,17 @@ class Exercise:
     Representation Invariants:
     - self.name != ""
     """
-    name: str
-    force: str
-    level: str
-    mechanic: Optional[str]
-    equipment: Optional[str]
-    muscles: list[str]
+    properties: list
     instructions: str
     category: str
     images: list[str]
 
-    def __init__(self, name: str, force: str, level: str, mechanic: Optional[str], equipment: Optional[str],
-                 muscles: Optional[str], instructions: str, category: str, images: list[str]) -> None:
+    def __init__(self, properties: list, instructions: str, category: str, images: list[str]) -> None:
         """Initialize an exercise with the given properties"""
-        self.name = name
-        self.force = force
-        self.level = level
-        self.mechanic = mechanic
-        self.equipment = equipment
-        self.muscles = muscles
+        self.properties = properties
         self.instructions = instructions
         self.category = category
         self.images = images
-
-    def get_properties(self) -> list:
-        """Return a list that contains all the nessesary properties we need"""
-        lst = [self.force, self.level, self.mechanic, self.equipment, self.muscles]
-        return lst
 
 
 def parse_list_field(field_str: str) -> list:
@@ -73,16 +67,21 @@ def get_all_exercises() -> list[Exercise]:
         # primaryMuscles,secondaryMuscles,instructions,category,images,id
         for row in reader:
             # Create an Exercise instance.
+            name = row[1]
+            force = row[2]
+            level = row[3]
+            mechanic = row[4] if row[4] else None
+            equipment = row[5] if row[5] else None
+            muscles = parse_list_field(row[6]) + parse_list_field(row[7])
+            instructions = row[8]
+            category = row[9]
+            images = parse_list_field(row[10])
+            lst = [name, force, level, mechanic, equipment, muscles]
             exercise = Exercise(
-                name=row[1],
-                force=row[2],
-                level=row[3],
-                mechanic=row[4] if row[4] else None,
-                equipment=row[5] if row[5] else None,
-                muscles=parse_list_field(row[6]) + parse_list_field(row[7]),
-                instructions=row[8],
-                category=row[9],
-                images=parse_list_field(row[10])
+                properties=lst,
+                instructions=instructions,
+                category=category,
+                images=images
             )
             exercises.append(exercise)
     return exercises
@@ -98,17 +97,18 @@ def load_lists(muscles_lst: list[str], exercise_name: str, graph: Graph) -> None
 def create_graph(exercises: list[Exercise]) -> Graph:
     """Return the graph for our datasets"""
     graph = Graph()
-    property_list = ["force", "level", "mechanic", "equipment", "primary_muscles", "secondary_muscles"]
+    property_list = ["name", "force", "level", "mechanic", "equipment", "primary_muscles", "secondary_muscles"]
     for exercise in exercises:
-        graph.add_vertex(exercise.name, "name")
-        lst = exercise.get_properties()
-        for index in range(len(lst)):
+        lst = exercise.properties
+        exercise_name = exercise.properties[0]
+        graph.add_vertex(exercise_name, "name")
+        for index in range(1, len(lst)):
             the_property = lst[index]
             if isinstance(list, the_property):
-                load_lists(the_property, exercise.name, graph)
+                load_lists(the_property, exercise_name, graph)
             elif the_property is not None:
                 graph.add_vertex(the_property, property_list[index])
-                graph.add_edge(exercise.name, the_property)
+                graph.add_edge(exercise_name, the_property)
     return graph
 
 
