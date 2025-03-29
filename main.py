5e95006551b7d1bcc1392df617a88731b4fbb037
Lou-Meixuan <meixuan.lou@mail.utoronto.ca>
2025-03-29 17:28:58 -0400

    Create the main file for TA

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-import data_loader
-import weight_calculation
+"""CSC111 Winter 2025 Project2 - Recommendation System For Gym Exercises: Workout Wizard
+
+Module Description
+==================
+This is the main file for our project
+
+Copyright and Usage Information
+===============================
+This file is created solely by students (Meixuan Lou, Zimo Huang, Hongyi Mei, Yunji Hwang) taking CSC111 at the
+University of Toronto St. George campus. All forms of distribution of this code, whether as given or with any changes,
+are expressly prohibited.
+
+This file is Copyright (c) 2025 CSC111 Student Team: Meixuan Lou, Zimo Huang, Hongyi Mei, Yunji Hwang
+"""
+import tkinter as tk
+from user_interface import ExerciseRecommendationApp
+
+root = tk.Tk()
+app = ExerciseRecommendationApp(root)
+root.mainloop()
